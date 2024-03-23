3f9d0dca357c0307b5f27d3e8b5320dd22cf4ee5
Hinata Tezuka <hinata.tezka.foreign@gmail.com>
2024-03-23 18:06:11 +0900

    update

--- free.cpp
+++ free.cpp
@@ -1,35 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <iomanip>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-long double culc_rate(int a, int b) {
-    return static_cast<long double>(a) / (a + b);
-}
-
 int main() {
-    int n;
-    cin >> n;
-
-    map<long double, vector<int>> res;
-
-    for (int i = 0; i < n; ++i) {
-        int a, b;
-        cin >> a >> b;
-        long double score = culc_rate(a, b);
-        res[score].push_back(i + 1);
-    }
-
-    map<long double, vector<int>, greater<long double>> sorted_res(res.begin(), res.end());
-
-    for (const auto& item : sorted_res) {
-        for (int val : item.second) {
-            cout << val << " ";
-        }
-    }
-    cout << endl;
 
     return 0;
 }
