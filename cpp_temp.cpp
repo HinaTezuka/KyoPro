8e6ac58fa1b43ce6a97982fe90e7784f5b14cd18
Hinata Tezuka <hinata.tezka.foreign@gmail.com>
2024-03-25 15:40:24 +0900

    update

--- cpp_temp.cpp
+++ cpp_temp.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-void print_element(int x) { // std::for_each(vec.begin(), vec.end(), print_element);
-    std::cout << x << " ";
+void print_element(int x) { // for_each(vec.begin(), vec.end(), print_element);
+    cout << x << " ";
+}
+
+void print_element2(const vector<vector<int>>& matrix) {
+    for (const auto& row : matrix) {
+        for (int x : row) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
 }
 
 int main() {
--- free.cpp
+++ free.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#include <numeric>
+
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
@@ -6,7 +8,34 @@ void print_element(int x) { // for_each(vec.begin(), vec.end(), print_element);
     cout << x << " ";
 }
 
-int main() {
+// void print_element2(const vector<vector<int>>& matrix) {
+//     for (const auto& row : matrix) {
+//         for (int x : row) {
+//             cout << x << " ";
+//         }
+//         cout << endl;
+//     }
+// }
 
+int main() {
+    // int A[9][9];
+    vector<vector<int> > A(9, vector<int>(9));
+    rep(i, 9){
+        rep(j, 9){
+            cin >> A[i][j];
+        }
+    }
+    for(int i=0; i<9; i++){
+        for(int j=0; j<9; j++){
+            cout << A[i][j] << "\n";
+        }
+    }
+    // rep(i, 9){
+    //     int tot = reduce(A[i].begin(), A[i].end());
+    //     if (tot != 45) {
+    //         cout << 'No' << "\n";
+    //         exit(0);
+    //     }
+    // }
     return 0;
 }
