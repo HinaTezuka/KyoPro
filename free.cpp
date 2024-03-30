7091fe18d8b6671208607dc196f240667e6a8f30
Hinata Tezuka <hinata.tezka.foreign@gmail.com>
2024-03-30 17:00:51 +0900

    update

--- free.cpp
+++ free.cpp
@@ -6,42 +6,64 @@ void print_element(int x) { // for_each(vec.begin(), vec.end(), print_element);
     cout << x << " ";
 }
 
-int a[19], b[19], c[19];
-vector<int> G[19];
+// 重み付きグラフの隣接リスト用
+struct Edge {
+    int to;
+    int weight;
+    Edge(int t, int w) : to(t), weight(w) { };
+};
 
 int main() {
     // 入力
     int n, m;
     cin >> n >> m;
-    for(int i = 1; i < m+1; i++) cin >> a[i] >> b[i] >> c[i];
-    // 連結行列
-    for(int i = 1; i < m+1; i++){
-        G[a[i]].push_back(b[i]);
-        G[b[i]].push_back(a[i]);
+    
+    // 連結リスト
+    vector<vector<Edge>> G(n+1);
+    for(int i = 0; i < m; i++){
+        int from, to, weight;
+        cin >> from >> to >> weight;
+        G[from].push_back(Edge(to, weight));
+        G[to].push_back(Edge(from, weight));
     }
 
-    // 各町にたいして有効pathを求める
-    int ans = -1;
+    // 各街に対して BFS を行い、最大のコストを求める
+    int ans = 0;
     for (int i = 1; i < n+1; i++){
+        if(G[i].empty()) continue;
+        int visited[30]; // 訪問済みの街かどうか
+        for(int i = 1; i < n+1; i++) visited[i] = -1;
         queue<int> Q;
         Q.push(i);
-        int dist[n+1];
-        for(int i = 1; i < n+1; i++) dist[i] = -1;
-        dist[i] = 0;
-        int tot = 0; // 合計コスト
-        // bfs
+        visited[i] += 1;
+        // BFS
+        vector<int> p;
+        p.push_back(i);
         while(!Q.empty()){
             int pos = Q.front(); Q.pop();
-            tot += c[pos];
             for(int j = 0; j < G[pos].size(); j++){
-                int to = G[pos][j];
-                if (dist[to] == -1){
-                    dist[to] = dist[pos]+1;
+                int to = G[pos][j].to;
+                if (visited[to] == -1){
+                    visited[to] += 1;
                     Q.push(to);
+                    p.push_back(to);
                 }
             }
         }
-        ans = max(ans, tot);
+        do{
+            int tot = 0;
+            for(int i = 0; i < p.size()-1; i++){
+                if (G[p[i]].empty()) continue;
+                for(int j = 0; j < G[p[i]].size(); j++){
+                    if(G[p[i]][j].to == p[i+1]){
+                        tot += G[p[i]][j].weight;
+                        break;
+                    }
+                }
+            }
+            ans = max(ans, tot);
+        }
+        while(next_permutation(p.begin(), p.end()));
     }
 
     cout << ans << "\n";
