3683dbebd6f037841a751eaeeb2a7a4332aca7d2
Hinata Tezuka <hinata.tezka.foreign@gmail.com>
2024-03-31 15:32:38 +0900

    update

---
+++ ABC/abc347/b.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+void print_element(int x) { // for_each(vec.begin(), vec.end(), print_element);
+    cout << x << " ";
+}
+
+int main() {
+    
+    
+    return 0;
+}
---
+++ ABC/abc347/c.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+void print_element(int x) { // for_each(vec.begin(), vec.end(), print_element);
+    cout << x << " ";
+}
+
+int main() {
+    // 入力
+    int n, a, b;
+    cin >> n >> a >> b;
+    vector<int> d(n+1);
+    for(int i = 1; i < n+1; i++) cin >> d[i];
+
+    int tot = a + b;
+    bool f = true;
+    for (int i = 1; i < n+1; i++){
+        if(d[i] <= a) continue;
+        int amari = d[i] % tot;
+        if((amari > a && amari <= b)) {
+            f = false;
+            break;
+        }
+    }
+    
+
+    if (f) cout << "Yes" << "\n";
+    else cout << "No" << "\n";
+
+    return 0;
+}
